9a950a7a9427813ee259dd8174c7e7269b59bf05
Ariane Deschenes Foucher <ariane.ariane@telus.com>
2017-03-01 22:21:52 -0500

    why not

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 class Header extends React.Component {
 
-
   render() {
     return (
         <div className="header">
-            <span id="see-more" onClick({Header})> see more | </span>
+            <span id="see-more"> see more | </span>
             <span id="feel-more"> feel more </span>
         </div>
     )
