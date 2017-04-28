d41271191e971c8d44ba6dce6b45e1b3d145ebbc
Ariane Deschenes Foucher <ariane.ariane@telus.com>
2017-04-28 19:05:15 -0400

    Added slideshow effect to left side images

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 $( document ).ready(function() {
-  $( "#see-more" ).on({
+  $( "#dribbble" ).on({
       mouseenter: function() {
         $( '#image' ).removeClass( "show-me" );
         $( '#image' ).addClass( "hide-me" );
-        $( '#dribbble' ).removeClass( "hide-me" );
-        $( '#dribbble' ).addClass( "show-me" );
       },
       mouseleave: function() {
           $( '#image' ).removeClass( "hide-me" );
           $( '#image' ).addClass( "show-me" );
-          $( '#dribbble' ).removeClass( "show-me" );
-          $( '#dribbble' ).addClass( "hide-me" );
       }
   });
-  $( "#feel-more" ).on({
+  $( "#instagram" ).on({
       mouseenter: function() {
         $( '#image' ).removeClass( "show-me" );
         $( '#image' ).addClass( "hide-me" );
-        $( '#instagram' ).removeClass( "hide-me" );
-        $( '#instagram' ).addClass( "show-me" );
       },
       mouseleave: function() {
           $( '#image' ).removeClass( "hide-me" );
           $( '#image' ).addClass( "show-me" );
-          $( '#instagram' ).removeClass( "show-me" );
-          $( '#instagram' ).addClass( "hide-me" );
       }
   });
+
+});
+jQuery(function($){
+
+  // Cycle plugin
+  $('.slides').cycle({
+    fx:     'none',
+    speed:   150,
+    timeout: 5
+  }).cycle("pause");
+
+  // Pause &amp; play on hover
+  $('#instagram').hover(function(){
+    $('.slideshow-block').find('.slides').addClass('active').cycle('resume');
+  }, function(){
+    $('.slideshow-block').find('.slides').removeClass('active').cycle('pause');
+  });
+
+  $('#dribbble').hover(function(){
+    $('.slideshow-block').find('.slides').addClass('active').cycle('resume');
+  }, function(){
+    $('.slideshow-block').find('.slides').removeClass('active').cycle('pause');
+  });
+
 });
